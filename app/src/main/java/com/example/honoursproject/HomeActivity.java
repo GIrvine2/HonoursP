11224591fabb4e3d47a134d31c01ac814c7ffa16
GIrvine2 <55537297+GIrvine2@users.noreply.github.com>
2021-02-22 20:28:59 +0000

    Added Side Nav Drawer

--- app/src/main/java/com/example/honoursproject/HomeActivity.java
+++ app/src/main/java/com/example/honoursproject/HomeActivity.java
@@ -1,19 +1,27 @@
 package com.example.honoursproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    TextView textView2;
-    Button logout;
+    private DrawerLayout drawer;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
@@ -23,15 +31,57 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        textView2 = findViewById(R.id.textView2);
-        logout = findViewById(R.id.btnLogout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        logout.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            Intent signout = new Intent(HomeActivity.this, MainActivity.class);
-            startActivity(signout);
-            finish();
-        });
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userid = user.getEmail();
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(userid);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent home = new Intent(this, HomeActivity.class);
+                startActivity(home);
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingsFragment()).commit();
+                break;
+            case R.id.nav_favorite:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FavouriteFragment()).commit();
+                break;
+            case R.id.nav_signout:
+                FirebaseAuth.getInstance().signOut();
+                Intent signOut = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(signOut);
+                finish();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
