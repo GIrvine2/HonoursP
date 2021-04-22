c4ed4a1c0e87561c1d96ae11fb3693735c84e91c
GIrvine2 <55537297+GIrvine2@users.noreply.github.com>
2021-04-22 22:20:34 +0100

    Display order and allow users to clear basket added.

--- app/src/main/java/com/example/honoursproject/BasketFragment.java
+++ app/src/main/java/com/example/honoursproject/BasketFragment.java
@@ -1,6 +1,7 @@
 package com.example.honoursproject;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,23 +14,32 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.view.menu.MenuView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class BasketFragment extends Fragment {
 
     View view;
+    ListView listView;
     Button confirmBtn, clearBtn;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     String userID = user.getUid();
+    ArrayList<String> arrayList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
     private DatabaseReference order = db.getReference().child("Basket").child(userID);
 
@@ -39,6 +49,39 @@ public class BasketFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_basket, container, false);
         confirmBtn = (Button) view.findViewById(R.id.confirmBtn);
         clearBtn = (Button) view.findViewById(R.id.clearBtn);
+        listView = (ListView) view.findViewById(R.id.listView);
+
+        order.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (getActivity() != null) {
+                    String value = snapshot.getValue(String.class);
+                    arrayList.add(value);
+                    arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrayList);
+                    listView.setAdapter(arrayAdapter);
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
@@ -47,6 +90,8 @@ public class BasketFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 order.removeValue();
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                startActivity(intent);
             }
         });
 
